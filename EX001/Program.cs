93a90b338f0f1c56a3436456fdda66f37570e2e4
Lucas Roldão Cardoso <lucasroldao2802@gmail.com>
2026-03-07 18:28:39 -0300

    Atualização

--- EX001/Program.cs
+++ EX001/Program.cs
@@ -6,15 +6,19 @@ namespace CursoBalta
 		static void Main(string[] args)
 		{
 			int inteiro = 100;
-			float real = 25.5f;
+			float real = 25.8f;
 
-			/* real = inteiro;  100.0f
-			 Console.WriteLine(real); */
-
-			real = inteiro;
-			inteiro = (int)real; 
-			Console.WriteLine(real);
+			// real = inteiro;  100.0f
+			// Console.WriteLine(real); 
+			// real = inteiro;
+			// inteiro = (int)real; 
+			// string valorReal = real.ToString();
+			// inteiro = int.Parse(valorReal);
+			// inteiro = Convert.ToInt32(real); 
 			
+			//Console.WriteLine(valorReal);
+			Console.WriteLine(inteiro);
+			Console.WriteLine(Convert.ToBoolean(255));
 		}
 	}
 }
