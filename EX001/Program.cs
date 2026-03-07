f43a108da6d1f450a8d00b761b562762fec47a5a
Lucas Roldão Cardoso <lucasroldao2802@gmail.com>
2026-03-07 18:11:35 -0300

    começo das atividades

---
+++ EX001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CursoBalta
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int inteiro = 100;
+			float real = 25.5f;
+
+			/* real = inteiro;  100.0f
+			 Console.WriteLine(real); */
+
+			real = inteiro;
+			inteiro = (int)real; 
+			Console.WriteLine(real);
+			
+		}
+	}
+}
